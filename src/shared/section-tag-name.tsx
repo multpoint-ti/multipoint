adcf9a2a53cda6668433b71462cf259e40089763
mandis-ncs <amnd.castro@gmail.com>
2025-12-07 21:15:55 -0300

    fix build

--- src/shared/section-tag-name.tsx
+++ src/shared/section-tag-name.tsx
@@ -1,10 +1,11 @@
 type SectionTagNameProps = {
   text: string;
+  className?: string;
 };
 
-const SectionTagName = ({ text }: SectionTagNameProps) => {
+const SectionTagName = ({ text, className }: SectionTagNameProps) => {
   return (
-    <p className="uppercase font-medium text-xs md:text-sm text-blue-ignition border-b-2 border-b-red-amber-torque w-fit h-fit">
+    <p className={`uppercase font-medium text-xs md:text-sm border-b-2 border-b-red-amber-torque w-fit h-fit ${className || 'text-blue-ignition'}`}>
       {text}
     </p>
   );
